d6cb15df7c4bd80c7e26d357153ddbcfd65eeb48
Junyeop Kim <junykim@c2r6s3.42seoul.kr>
2022-05-02 20:53:17 +0900

    finished read_map

--- src/draw.c
+++ src/draw.c
@@ -6,29 +6,29 @@
 /*   By: junykim <junykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 19:35:36 by junykim           #+#    #+#             */
-/*   Updated: 2022/05/02 20:38:56 by junykim          ###   ########.fr       */
+/*   Updated: 2022/05/02 20:41:20 by junykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
-void	draw(t_fdf *data)
-{
-	int	x;
-	int	y;
-
-	y = 0;
-	while (y < data->column)
-	{
-		x = 0;
-		while (x < data->row)
-		{
-			if (x < data->row - 1)
-				bresenham(x, y, x + 1, y, data);
-			if (y < data->column - 1)
-				bresenham(x, y, x, y + 1, data);
-			x++;
-		}
-		y++;
-	}
-}
+/** void	draw(t_fdf *data) */
+/** { */
+/**     int	x; */
+/**     int	y; */
+/**  */
+/**     y = 0; */
+/**     while (y < data->column) */
+/**     { */
+/**         x = 0; */
+/**         while (x < data->row) */
+/**         { */
+/**             if (x < data->row - 1) */
+/**                 bresenham(x, y, x + 1, y, data); */
+/**             if (y < data->column - 1) */
+/**                 bresenham(x, y, x, y + 1, data); */
+/**             x++; */
+/**         } */
+/**         y++; */
+/**     } */
+/** } */
--- src/main.c
+++ src/main.c
@@ -6,32 +6,34 @@
 /*   By: junykim <junykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 16:47:12 by junykim           #+#    #+#             */
-/*   Updated: 2022/05/02 19:16:48 by junykim          ###   ########.fr       */
+/*   Updated: 2022/05/02 20:48:28 by junykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
+#include <stdio.h>
 
 /** what is mlx_ptr? */
-int	deal_key(int key, t_fdf *data)
-{
-	/** ft_printf("%d\n", key); */
-	if (key == 126)
-		data->shift_y -= 10;
-	if (key == 125)
-		data->shift_y += 10;
-	if (key == 124)
-		data->shift_x += 10;
-	if (key == 123)
-		data->shift_x -= 10;
-	mlx_clear_window(data->mlx_ptr, data->win_ptr);
-	draw(data);
-	return (0);
-}
+/** int	deal_key(int key, t_fdf *data) */
+/** { */
+/**     [> ft_printf("%d\n", key); <] */
+/**     if (key == 126) */
+/**         data->shift_y -= 10; */
+/**     if (key == 125) */
+/**         data->shift_y += 10; */
+/**     if (key == 124) */
+/**         data->shift_x += 10; */
+/**     if (key == 123) */
+/**         data->shift_x -= 10; */
+/**     mlx_clear_window(data->mlx_ptr, data->win_ptr); */
+/**     draw(data); */
+/**     return (0); */
+/** } */
 
 int	main(int ac, char **av)
 {
 	t_fdf	*data;
+	int i = 0, j = 0;
 
 	(void)ac;
 	/** if (ac == 1) */
@@ -42,12 +44,24 @@ int	main(int ac, char **av)
 	if (!data)
 		return (-1);
 	read_file(av[1], data);
-	data->mlx_ptr = mlx_init();
-	data->win_ptr = mlx_new_window(data->mlx_ptr, WINDOW_X_LENGTH, WINDOW_Y_LENGTH, WINDOW_TITLE);
-	data->zoom = 20;
-	/** bresenham(10, 10, 600, 300, data); */
-	draw(data);
-	mlx_key_hook(data->win_ptr, deal_key, NULL);//?
-	mlx_loop(data->mlx_ptr);
+	/** data->mlx_ptr = mlx_init(); */
+	/** data->win_ptr = mlx_new_window(data->mlx_ptr, WINDOW_X_LENGTH, WINDOW_Y_LENGTH, WINDOW_TITLE); */
+	/** data->zoom = 20; */
+	/** [> bresenham(10, 10, 600, 300, data); <] */
+	/** [> draw(data); <] */
+	/** mlx_key_hook(data->win_ptr, deal_key, NULL);//? */
+	/** mlx_loop(data->mlx_ptr); */
+	i = 0;
+	while (i < data->column)
+	{
+		j = 0;
+		while (j < data->row)
+		{
+			printf("%3d", data->z_matrix[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
 	return (0);
 }
